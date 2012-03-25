3a31e756d212ec766b9e3dcc1554e37edf88bf8a
Konstantin Zamyakin <dev@weblab.pro>
2012-03-25 16:26:56 +0400

    some cosmetics fixes

--- AdobeConnectClient.class.php
+++ AdobeConnectClient.class.php
@@ -1,42 +1,109 @@
 <?php
-class AdobeConnectClient {
-	const USERNAME = '';
-	const PASSWORD = '';
-	const BASE_DOMAIN = 'https://example.adobeconnect.com/api/'; //вынести в конфиги
-	const ROOT_FOLDER_ID = 0; //ИД корневой папки
-	private $cookie;
-	private $curl;
-	private $is_authorized = false;
-	private $is_debug = false;
+/*
+ * AdobeConnect 8 api client
+ * @see https://github.com/sc0rp10/AdobeConnect-php-api-client
+ * @see http://help.adobe.com/en_US/connect/8.0/webservices/index.html
+ * @version 0.1a
+ *
+ * Copyright 2012, sc0rp10
+ * https://weblab.pro
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/MIT
+ *
+ */
 
+class AdobeConnectClient {
+	/**
+	 * adobe connect username
+	 */
+	const USERNAME = '';
+
+	/**
+	 * @const
+	 * adobe connect password
+	 */
+	const PASSWORD = '';
+
+	/**
+	 * @const
+	 * your personally api URL
+	 */
+	const BASE_DOMAIN = 'https://example.adobeconnect.com/api/';
+
+	/**
+	 * @const
+	 * your personally root-folder id
+	 * @see http://forums.adobe.com/message/2620180#2620180
+	 */
+	const ROOT_FOLDER_ID = 0; //root folder id
+
+	/**
+	 * @var string filepath to cookie-jar file
+	 */
+	private $cookie;
+
+	/**
+	 * @var resource
+	 */
+	private $curl;
+
+	/**
+	 * @var bool
+	 */
+	private $is_authorized = false;
+
+	/**
+	 *
+	 */
 	public function __construct () {
-		$this->cookie = sfConfig::get('sf_cache_dir'). DIRECTORY_SEPARATOR .'cookie_'.time().'.txt'; // вынести в параметр
+		$this->cookie = sys_get_temp_dir().DIRECTORY_SEPARATOR.'cookie_'.time().'.txt'; // вынести в параметр
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		curl_setopt($ch, CURLOPT_REFERER, 'http://profile.weblab.pro/');
+		curl_setopt($ch, CURLOPT_REFERER, self::BASE_DOMAIN);
 		curl_setopt($ch, CURLOPT_COOKIEJAR, $this->cookie);
 		curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookie);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		$this->curl = $ch;
 		$this->makeAuth();
 	}
-	public function enableDebug() {
-		$this->is_debug = true;
-		return $this;
-	}
+
+	/**
+	 * make auth-request with stored username and password
+	 *
+	 * @return AdobeConnectClient
+	 */
 	public function makeAuth() {
-		$result = $this->makeRequest('login', 
+		$this->makeRequest('login',
 			array(
-				'login' => self::USERNAME, 
+				'login'    => self::USERNAME,
 				'password' => self::PASSWORD
 			)
 		);
 		$this->is_authorized = true;
 		return $this;
 	}
+
+	/**
+	 * get common info about current user
+	 *
+	 * @return array
+	 */
 	public function getCommonInfo() {
 		return $this->makeRequest('common-info');
 	}
+
+	/**
+	 * create user
+	 *
+	 * @param string $email
+	 * @param string $password
+	 * @param string $first_name
+	 * @param string $last_name
+	 * @param string $type
+	 *
+	 * @return array
+	 */
 	public function createUser ($email, $password, $first_name, $last_name, $type = 'user') {
 		$result = $this->makeRequest('principal-update', 
 			array(
@@ -50,28 +117,51 @@ class AdobeConnectClient {
 		);
 		return $result;
 	}
-	public function getInfoAboutMe() {
-		$result = $this->getCommonInfo();
-		$r = array();
-		$r['login'] = $result['common']['user']['login'];
-		$r['name'] = $result['common']['user']['name'];
-		return $result['common']['user']['@attributes'] + $r;
-	}
+
+
+	/**
+	 * @param string $email
+	 * @param bool   $only_id
+	 *
+	 * @return mixed
+	 *
+	 * @throws Exception
+	 *
+	 */
 	public function getUserByEmail($email, $only_id = false) {
-		$result = $this->makeRequest('principal-list', array('filter-email' => $email));
+		$result = $this->makeRequest('principal-list',
+			array(
+				'filter-email' => $email
+			)
+		);
 		if (empty($result['principal-list'])) {
-			throw new Exception('Пользователь не найден');
+			throw new Exception('Cannot find user');
 		}
 		if ($only_id) {
 			return $result['principal-list']['principal']['@attributes']['principal-id'];
 		}
 		return $result;
 	}
+
+	/**
+	 * update user fields
+	 *
+	 * @param string $email
+	 * @param array  $data
+	 *
+	 * @return mixed
+	 */
 	public function updateUser($email, array $data = array()) {
 		$principal_id = $this->getUserByEmail($email, true);
 		$data['principal-id'] = $principal_id;
 		return $this->makeRequest('principal-update', $data);
 	}
+
+	/**
+	 * get all users list
+	 *
+	 * @return array
+	 */
 	public function getUsersList() {
 		$users = $this->makeRequest('principal-list');
 		$result = array();
@@ -81,9 +171,23 @@ class AdobeConnectClient {
 		unset($result[$key]['@attributes']);
 		return $result;
 	}
-	public function getAllMeetings() {
-		$this->makeRequest('report-my-meetings');
+
+	/**
+	 * get all meetings
+	 *
+	 * @return array
+	 */public function getAllMeetings() {
+		return $this->makeRequest('report-my-meetings');
 	}
+
+	/**
+	 * create meeting-folder
+	 *
+	 * @param string $name
+	 * @param string $url
+	 *
+	 * @return array
+	 */
 	public function createFolder($name, $url) {
 		$result = $this->makeRequest('sco-update', 
 			array(
@@ -96,6 +200,18 @@ class AdobeConnectClient {
 		);
 		return $result['sco']['@attributes']['sco-id'];
 	}
+
+	/**
+	 * create meeting
+	 *
+	 * @param int    $folder_id
+	 * @param string $name
+	 * @param string $date_begin
+	 * @param string $date_end
+	 * @param string $url
+	 *
+	 * @return array
+	 */
 	public function createMeeting($folder_id, $name, $date_begin, $date_end, $url) {
 		$result = $this->makeRequest('sco-update', 
 			array(
@@ -109,22 +225,38 @@ class AdobeConnectClient {
 		);
 		return $result['sco']['@attributes']['sco-id'];
 	}
-	public function invitePeopleToMeeting($meeting_id, $email) {
+
+	/**
+	 * invite user to meeting
+	 *
+	 * @param int    $meeting_id
+	 * @param string $email
+	 *
+	 * @return mixed
+	 */
+	public function inviteUserToMeeting($meeting_id, $email) {
 		$user_id = $this->getUserByEmail($email, true);
 
-    	$result = $this->makeRequest('permissions-update', 
-    		array(
-	    		'principal-id'  => $user_id,
-	    		'acl-id'        => $meeting_id,
-	    		'permission-id' => 'view'
-	    	)
-    	);
-    	return $result;
-	}
+		$result = $this->makeRequest('permissions-update', 
+			array(
+				'principal-id'  => $user_id,
+				'acl-id'        => $meeting_id,
+				'permission-id' => 'view'
+			)
+		);
+		return $result;
+}
+
 	public function __destruct() {
 		@curl_close($this->curl);
 	}
-	private function makeRequest($action, array $params = array()) {
+
+	/**
+	 * @param       $action
+	 * @param array $params
+	 * @return mixed
+	 * @throws Exception
+	 */private function makeRequest($action, array $params = array()) {
 		$url = self::BASE_DOMAIN;
 		$url .= 'xml?action='.$action;
 		$url .= '&'.http_build_query($params);
@@ -132,7 +264,7 @@ class AdobeConnectClient {
 		$result = curl_exec($this->curl);
 		$xml = simplexml_load_string($result);
 
-		$json = json_encode($data);
+		$json = json_encode($xml);
 		$data = json_decode($json, TRUE); // nice hack!
 		
 		if (!isset($data['status']['@attributes']['code']) || $data['status']['@attributes']['code'] !== 'ok') {
